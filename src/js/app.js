1af0ab0ee57a84d2b60d5efa7035780cd616845f
Doug Friedman <dfriedman@DougFrimansMBP5.localdomain>
2018-03-14 16:22:47 -0500

    react updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,27 @@
-require('../css/main.css');
-import {
-  message,
-  setElementValue,
-  SimpleReact,
-  StatefulReact
-} from './main';
-import React from 'react';
-import ReactDOM from 'react-dom';
+// require('../css/main.css');
+// import {
+//   message,
+//   setElementValue,
+//   SimpleReact,
+//   StatefulReact
+// } from './main';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// ReactDOM.render(
+//   <StatefulReact />,
+//   document.getElementById('root')
+// );
 
-ReactDOM.render(
-  <StatefulReact />,
-  document.getElementById('root')
-);
+
+async function main() {
+  const apikey = '95c34ace-63f5-4d4a-9015-13cecb65f12d';
+  const query = 'north+korea';
+
+  let fetchResult = await fetch(`https://content.guardianapis.com/search?api-key=${apikey}&q=${query}`)
+  let parseResult = await fetchResult.json();
+
+  console.log(parseResult);
+}
+
+main();
--- src/js/main.js
+++ src/js/main.js
@@ -12,22 +12,23 @@ export class StatefulReact extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: "Hello from the great state of React"
+      num: 0
     };
   }
 
 
   render() {
+    console.log('rendering!');
     return (
       <div className="mainrow">
         <div className="row">
-          {this.state.message}
+          {this.state.num}
         </div>
         <div className="row">
           <button
             className="btn"
             onClick={() => {
-              this.setState({message: "new state"})}
+              this.setState({num: this.state.num+1})}
             }
           >
             Change
