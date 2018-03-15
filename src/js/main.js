8cbc88bd86c8343ac97fecefb7d5bbde6fcf0ad6
Doug Friedman <dfriedman@DougFrimansMBP5.localdomain>
2018-03-15 10:50:03 -0500

    basic guardian api

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,86 @@
 import React, { Component } from 'react';
 
-export const SimpleReact = () => {
-  return <div>Hello from JSX</div>;
-}
+const apikey = 'ad94cad8-dfd1-411b-bb75-0640b9fb4117';
+const baseUrl = 'https://content.guardianapis.com/search';
 
-async function callAPI(term, page = 1) {
-  const apikey = '95c34ace-63f5-4d4a-9015-13cecb65f12d';
 
-  let fetchResult = await fetch(`https://content.guardianapis.com/search?api-key=${apikey}&q=${term}&currentPage=${page}`)
-  return await fetchResult.json();
+export const GoButton = (props) => {
+  if(props.loading){
+    return <span id="goButton" style={{flex: 1, marginRight: '10px', marginLeft: '10px'}}>...</span>
+  }else{
+    return <button
+      onClick={props.clickHandler}
+      id="goButton"
+      style={{flex: 1, marginRight: '10px', marginLeft: '10px'}}>Go</button>
+  }
 }
 
 export class StatefulReact extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      num: 0
-    };
+      searchTerm: '',
+      currentPage: 1,
+      maxPage: 1,
+      articles: [],
+      loading: false
+    }
+
+    this.handleTyping = this.handleTyping.bind(this);
+    this.callout = this.callout.bind(this);
+  }
+
+  async callout() {
+    this.setState({loading: true});
+    let url = `${baseUrl}?api-key=${apikey}&q=${this.state.searchTerm}&page=${this.state.currentPage}`;
+
+    const response = await fetch(url);
+    const jsonResp = await response.json();
+    const data = jsonResp.response;
+    console.log(jsonResp);
+    this.setState({
+      currentPage: data.currentPage,
+      maxPage: data.pages,
+      articles: data.results,
+      loading: false
+    });
+  }
+
+  handleTyping(ev){
+    this.setState({
+      searchTerm: ev.target.value
+    });
   }
 
   render() {
-    console.log('rendering!');
+    console.log(this.state);
     return (
-      <div className="mainrow">
-        <div className="row">
-          {this.state.num}
-        </div>
-        <div className="row">
-          <button
-            className="btn"
-            onClick={() => {
-              this.setState({num: this.state.num+1})}
-            }
-          >
-            Change
-          </button>
+      <div class="container">
+        <div class="mainBody">
+          <div class="searchBar">
+            <i class="ion-search" style={{flex: 1, textAlign: 'center'}}></i>
+            <input
+              id="input"
+              type="text"
+              name="search"
+              placeholder = "Search"
+              style={{flex: 8, marginRight: '10px'}}
+              onChange={this.handleTyping}
+            />
+            <GoButton loading={this.state.loading} clickHandler={this.callout} />
+          </div>
+          <ol id="demo" style={{flex: 8, borderStyle: 'dashed', listStyleType: 'none'}}>
+            {this.state.articles.map( article => {
+              return (
+                <li><a href={article.webUrl} target="_blank">{article.webTitle}</a></li>
+              )
+            })}
+          </ol>
+          <div style={{display: 'flex', flexDirection: 'row', margin: '10px 10px 10px 10px'}}>
+            <button id="backButton" style={{flex: 1}}>Back</button>
+            <div id="pageNum" style={{flex: 9, textAlign: 'center'}}></div>
+            <button id="forwardButton" style={{flex: 1}}>Forward</button>
+          </div>
         </div>
       </div>
     )
